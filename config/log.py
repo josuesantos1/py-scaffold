72f0a3cbcf90158b088ac2da21aa3f5129771bf1
josue santos <josue.santos.dev@gmail.com>
2026-03-05 22:14:26 +0000

    chore: improve logging config

--- config/log.py
+++ config/log.py
@@ -1,6 +1,7 @@
 import logging
 
 import structlog
+from structlog.contextvars import merge_contextvars
 
 from config.settings import settings
 
@@ -15,11 +16,17 @@ def setup_logging():
 
     structlog.configure(
         processors=[
-            structlog.processors.TimeStamper(fmt="iso"),
+            merge_contextvars,
             structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
             structlog.processors.JSONRenderer(),
         ],
         wrapper_class=structlog.make_filtering_bound_logger(log_level),
         logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
     )
+
+    log = structlog.get_logger()
+    log.info("Logging configured", level=log_level)
