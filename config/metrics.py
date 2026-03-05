693ea03be1b2a8dc578332fd22c621e5d5bc2d62
josue santos <josue.santos.dev@gmail.com>
2026-03-05 15:58:06 -0300

    config: added metrics config

---
+++ config/metrics.py
@@ -0,0 +1,7 @@
+from prometheus_client import Counter, Histogram
+
+REQUEST_COUNT = Counter(
+    "http_requests_total", "Total HTTP requests", ["method", "endpoint", "status"]
+)
+
+REQUEST_LATENCY = Histogram("http_request_duration_seconds", "Request latency", ["endpoint"])
