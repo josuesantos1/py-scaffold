43afdaa5ff62f86698ae21a707805c554d405bc3
josue santos <josue.santos.dev@gmail.com>
2026-03-05 15:56:55 -0300

    config: added logging config

---
+++ config/log.py
@@ -0,0 +1,25 @@
+import logging
+
+import structlog
+
+from config.settings import settings
+
+
+def setup_logging():
+    log_level = getattr(logging, settings.log_level.upper(), logging.INFO)
+
+    logging.basicConfig(
+        format="%(message)s",
+        level=log_level,
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.add_log_level,
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
