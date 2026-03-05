135a3119c8def25263e762a8bf71c31031da34fe
josue santos <josue.santos.dev@gmail.com>
2026-03-05 16:02:27 -0300

    feat: added routes to admin

--- app/admin/view.py
+++ app/admin/view.py
@@ -1,8 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 router = APIRouter()
 
 
 @router.get("/")
-def metrics():
+def root():
     return {"admin": "Hello"}
+
+
+@router.get("/health")
+async def health():
+    return {"status": "healthy"}
+
+
+@router.get("/ready")
+async def ready():
+    return {"status": "ready"}
+
+
+# Metrics endpoint
+@router.get("/metrics")
+async def metrics():
+    return Response(content=generate_latest(), media_type=CONTENT_TYPE_LATEST)
